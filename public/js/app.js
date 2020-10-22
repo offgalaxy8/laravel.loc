0633277d2ee1920b00b476e9ea951eeed0d7bc26
offgalaxy8 <56636426+offgalaxy8@users.noreply.github.com>
2020-10-22 23:12:16 +0300

    change styles on home page

--- public/js/app.js
+++ public/js/app.js
@@ -19384,8 +19384,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! c:\Openserver\domains\laravel.loc\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! c:\Openserver\domains\laravel.loc\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\OSPanel\domains\laravel.loc\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\OSPanel\domains\laravel.loc\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,9 +3,11 @@
 @section('title_doc')Главная страница@endsection
 
 @section('content')
-    <h1>Главная страница</h1>
-    <h3>Проект работает через github!</h3>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto delectus doloremque exercitationem facilis, fugiat possimus voluptates voluptatibus! Architecto asperiores culpa dolorum laborum libero molestias, obcaecati omnis perspiciatis sit. Quae, sequi!</p>
+    <div class="home-text">
+        <h1>Главная страница</h1>
+        <h3>Проект работает через github!!</h3>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Architecto delectus doloremque exercitationem facilis, fugiat possimus voluptates voluptatibus! Architecto asperiores culpa dolorum laborum libero molestias, obcaecati omnis perspiciatis sit. Quae, sequi!</p>
+    </div>
 @endsection
 
 @section('aside')
